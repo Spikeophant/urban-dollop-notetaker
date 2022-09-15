2d3043c640f9d30a8c95a750a28e31284849ec16
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-15 13:45:07 -0700

    Add / route.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 //setup app.
 const app = express();
 
-app.use(express.static);
+app.use(express.static('./public'));
 
+app.get('/', path.join(__dirname, '/public/index.html'));
