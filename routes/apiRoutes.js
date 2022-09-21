522ae1767739298538866cab1d86d645180601f7
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-21 14:08:29 -0700

    Delete and show active note.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,18 +6,27 @@ const path = require('path');
 router.post('/api/notes', (req, res) => {
   const newNote = req.body;
   let notesList = db;
+  newNote.id = notesList.length + 1;
   notesList.push(newNote);
   console.log(`Writing notes ${notesList}`)
-  fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesList), err => console.log(err));
-  res.send('Thank you for your note!')
+  fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesList), err => err);
+  res.send('Thank you for your note!');
 });
 
-router.delete('/api/notes', (req, res) =>{
-  console.log(req.body);
+router.delete('/api/notes/*', (req, res) =>{
+  let notesList = db;
+  for (const note in notesList) {
+    if (notesList[note].id === Number(req.params[0])) {
+      notesList.splice(note, 1);
+      fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesList), err => err);
+      res.send('Note Deleted.');
+    }
+  }
+
 })
 
 router.use('/api/notes', (req, res) => {
-  res.json(db)
+  res.json(db);
 })
 
 module.exports = router;
